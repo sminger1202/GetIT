e6eae7444f0cabf601886fd7d5c5c54ff164be3a
sminger1202 <182082585@qq.com>
2016-01-03 11:35:30 +0800

    make it run

--- app/src/main/java/com/android/getit/sourceBean/InitBean.java
+++ app/src/main/java/com/android/getit/sourceBean/InitBean.java
@@ -7,4 +7,33 @@ import com.android.getit.netBeanLoader.baseNetBean.BaseNetBean;
  */
 public class InitBean extends BaseNetBean {
 
+    @Override
+    public String getSuffix() {
+        return null;
+    }
+
+    @Override
+    public String getUrl() {
+        return null;
+    }
+
+    @Override
+    public String toJson() {
+        return null;
+    }
+
+    @Override
+    public boolean parseJson(String json, boolean bLoadMore) {
+        return false;
+    }
+
+    @Override
+    public String getCachePath() {
+        return null;
+    }
+
+    @Override
+    public String getHttpMethod() {
+        return null;
+    }
 }
